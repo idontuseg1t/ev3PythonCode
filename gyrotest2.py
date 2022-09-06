6a0ea6878a254268637b6b15d9c4e9e4654a4fc8
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-09-06 13:03:24 +1000

    Update gyrotest2.py

--- gyrotest2.py
+++ gyrotest2.py
@@ -2,17 +2,27 @@
 
 from time import sleep
 # ev3dev stuff
-from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank, MoveSteering
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor import INPUT_2
-from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.led import Leds
 
 # variables
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+tank_steer = MoveSteering(OUTPUT_B, OUTPUT_C)
 tank_drive.gyro = GyroSensor()
 tank_drive.gyro.calibrate()
+tank_drive.sonic = UltrasonicSensor()
 
 while True:
-  if 
+  sleep(0.01)
+  motor_pair.on(steering=0, speed=10)
+  if ultrasonic_sensor.distance_centimeters < 6:
+    motor_pair.off()
+    tank_drive.turn_right(10, 90)
+    tank_drive.on_for_seconds(20,20,5)
+    tank_drive.turn_left(10,90)
+    
+    
