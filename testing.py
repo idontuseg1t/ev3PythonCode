0ef1d12eaf374dcfb52522bff76ce70c775f4c8a
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-09-06 11:12:59 +1000

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,6 +4,7 @@ from time import sleep
 # ev3dev stuff
 from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_2
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.sensor.lego import GyroSensor
 from ev3dev2.led import Leds
