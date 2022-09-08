e82f64f6e7251d62d5aa28a7858fce3dcb9b671a
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-09-08 14:38:51 +1000

    Update gyrotest.py

--- gyrotest.py
+++ gyrotest.py
@@ -18,9 +18,9 @@ tank_drive.touch = TouchSensor()
 
 while True:
   sleep(0.01)
-  motor_pair.on(steering=0, speed=10)
+  tank_steer.on(steering=0, speed=10)
   if tank_drive.touch.is_pressed:
-    motor_pair.off()
+    tank_steer.off()
     tank_drive.turn_right(10, 90)
     tank_drive.on_for_seconds(20,20,5)
     tank_drive.turn_left(10,90)
