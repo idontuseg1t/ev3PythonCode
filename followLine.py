dc5e4f95e936486f61f9a353f1058388b1d4ac66
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-09-08 13:28:36 +1000

    Create followLine.py

---
+++ followLine.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from time import sleep
+# ev3dev stuff
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank, MoveSteering
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_2
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.led import Leds
+
+# variables
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+tank_steer = MoveSteering(OUTPUT_B, OUTPUT_C)
+tank_drive.gyro = GyroSensor()
+tank_drive.gyro.calibrate()
+tank_drive.color = ColorSensor()
+
+
+while True:
+    sleep(0.01)
+    if tank_drive.color.color == 1:
+        tank_steer.on(steering=0, speed=10)
+    else:
+        tank_steer.off()
+        tank_steer.on_for_rotations(0,-10,0.1)
+        tank_drive.turn_left(10,20)
+        if tank_drive.color.color == 1:
+            tank_drive.turn_right(10,20)
+            tank_steer.on_for_rotations(0,10,0.2)
+            tank_drive.turn_right(10,90)
+        else:
+            tank_drive.turn_right(10,20)
+            tank_steer.on_for_rotations(0,10,0.2)
+            tank_drive.turn_left(10,90)
