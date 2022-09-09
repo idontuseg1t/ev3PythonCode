5df3a16b8d45dbe82036e14bc7f2b933a3c9ba86
idontuseg1t <111327977+idontuseg1t@users.noreply.github.com>
2022-09-09 11:19:38 +1000

    Create straightLine.py

---
+++ straightLine.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from time import sleep
+# ev3dev stuff
+from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank, SpeedRPM
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_2
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.led import Leds
+
+# variables
+tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+tank_drive.gyro = GyroSensor()
+tank_drive.gyro.calibrate()
+
+#move forward
+tank_drive.on_for_seconds(SpeedRPM(200), 10)
+
